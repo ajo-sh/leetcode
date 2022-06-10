6dd58746091afaf57fb1bd071fb4cd48d71e76b1
ajo-sh <101784565+ajo-sh@users.noreply.github.com>
2022-06-10 12:02:15 +0200

    Update reverseinteger.py

--- reverseinteger.py
+++ reverseinteger.py
@@ -3,7 +3,7 @@
 # Given a signed 32-bit integer x, return x with its digits reversed. 
 # If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 class Solution:
-    def reverse(self, x: int) -> int:
+    def reverse(self):
         if x<0:
             y=-1*int(str(-x)[::-1]) # Reversing negative integer
         else:
